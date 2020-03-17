3858438bddbc440b41d5cd11464398bb56d148f4
沈杰 <shenjie@yunhx.net>
2020-03-17 15:26:48 +0800

    fix

--- server-sentinel/src/main/java/server/sentinel/TestController.java
+++ server-sentinel/src/main/java/server/sentinel/TestController.java
@@ -1,5 +1,6 @@
 package server.sentinel;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ class TestController {
     @Autowired
     private EchoService echoService;
 
+    @SentinelResource("hello")
     @GetMapping(value = "/")
     public String hello() {
         return echoService.echo("abc");
