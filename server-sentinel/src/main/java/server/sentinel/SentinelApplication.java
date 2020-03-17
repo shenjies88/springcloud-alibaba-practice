c02f21af296d193cb8d41dd7cc1887d7e1acd2c5
沈杰 <shenjie@yunhx.net>
2020-03-17 11:54:58 +0800

    zuul

--- server-sentinel/src/main/java/server/sentinel/SentinelApplication.java
+++ server-sentinel/src/main/java/server/sentinel/SentinelApplication.java
@@ -2,8 +2,10 @@ package server.sentinel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableZuulProxy
 @EnableFeignClients
 @SpringBootApplication
 public class SentinelApplication {
